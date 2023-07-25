01571ebfde1cac0908202dffe4f4f54016f2b4d5
Alperen_Lermi <alperenlermi0@gmail.com>
2023-07-25 21:45:14 +0300

    V0.1
    React and Three.js Testing
    
    - Three.js Küp testleri oluşturuldu
    - Three Js model yükleme testleri yapıldı

---
+++ src/components/RenderDisplay.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function RenderDisplay(){
+    return(
+        <div>Render Display</div>
+    )
+}
+export default RenderDisplay;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+ function SideBar(){
+    return (
+        <div className="container-fluid">
+            <h4 className="bg-danger">SideBar</h4>
+        </div>
+    )
+ }
+export default SideBar;
